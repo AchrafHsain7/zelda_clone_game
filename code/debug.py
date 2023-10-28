a47904bb4bd6d1f13e12d7a4a314e02f3ccbf07d
AHProgramer <95654726+AHProgramer@users.noreply.github.com>
2023-10-28 11:01:09 +0100

    debug working?

--- code/debug.py
+++ code/debug.py
@@ -2,7 +2,7 @@ import pygame
 pygame.init()
 font = pygame.font.Font(None, 30)
 
-def debug(info, x=10, y=10):
+def debug(info, x=20, y=20):
     display_surface = pygame.display.get_surface()
     debug_surf = font.render(str(info), True, 'White')
     debug_rect = debug_surf.get_rect(topleft=(x,y))
