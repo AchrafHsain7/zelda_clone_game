2bba8fe3aa974ba0c3df0bfe13860610c21905dd
AHProgramer <95654726+AHProgramer@users.noreply.github.com>
2023-10-28 12:45:59 +0100

    added a freacking camera

--- code/level.py
+++ code/level.py
@@ -1,4 +1,6 @@
+from typing import Sequence, Union
 import pygame
+from pygame.sprite import Sprite
 from settings import *
 from tile import Tile
 from player import Player
@@ -11,7 +13,7 @@ class Level:
         self.display_surface = pygame.display.get_surface()
 
         #the sprite groups
-        self.visible_sprites = pygame.sprite.Group()
+        self.visible_sprites = YSortCameraGroup()
         self.obstacle_sprites = pygame.sprite.Group()
 
         #create the map
@@ -25,10 +27,28 @@ class Level:
                 if col == 'x':
                     Tile((x, y), [self.visible_sprites, self.obstacle_sprites])
                 elif col == 'p':
-                    self.player = Player((x, y), [self.visible_sprites])
+                    self.player = Player((x, y), [self.visible_sprites], self.obstacle_sprites)
 
     #update and draw the game
     def run(self):
-        self.visible_sprites.draw(self.display_surface)
+        self.visible_sprites.custom_draw(self.player)
         self.visible_sprites.update()
-        debug(self.player.direction)
+
+
+class YSortCameraGroup(pygame.sprite.Group):
+    def __init__(self):
+        super().__init__()
+        self.display_surface = pygame.display.get_surface()
+        #getting half of the screen width and height to keep player in the middle of the screen always 
+        self.half_width = self.display_surface.get_size()[0] // 2
+        self.half_height = self.display_surface.get_size()[1] // 2
+        self.offset = pygame.math.Vector2()
+
+    def custom_draw(self, player):
+        #getting the offset
+        self.offset.x = -player.rect.x + self.half_width
+        self.offset.y = -player.rect.y + self.half_height
+
+        for sprite in self.sprites():
+            offset_rect = sprite.rect.topleft + self.offset
+            self.display_surface.blit(sprite.image, offset_rect)
